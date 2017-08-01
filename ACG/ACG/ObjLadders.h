b0efc8fe5211e7a264417ebb03088f50cd14ee01
kitayama <h1201615003@hiratagakuen.onmicrosoft.com>
2017-08-01 16:29:13 +0900

    Ladders(ハシゴ)のcppを追加

--- ACG/ACG/ObjLadders.h
+++ ACG/ACG/ObjLadders.h
@@ -5,17 +5,17 @@
 //使用するネームスペース
 using namespace GameL;
 
-//オブジェクト：主人公
+//オブジェクト：ハシゴ
 class CObjLadders :public CObj
 {
 public:
 	CObjLadders(float x, float y);
 	~CObjLadders() {};	//デストラクタ
-	void Init();	//イニシャライズ
-	void Action();	//アクション
-	void Draw();	//ドロー
+	void Init();	    //イニシャライズ
+	void Action();	    //アクション
+	void Draw();	    //ドロー
 
 private:
-	float m_x;		//位置
-	float m_y;
+	float m_x;		//X座標
+	float m_y;      //Y座標
 };
