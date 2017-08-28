da9a578e47247f9157420055f0006b169c34b166
nakano <h1201615016@hiratagakuen@onmicrosoft.com>
2017-08-28 15:55:44 +0900

    BOSSの変数たちを変更 BOSSに摩擦と重力を追加 ENEMYの摩擦と重力が消されていたので追加

--- ACG/ACG/ObjBossEnemy.h
+++ ACG/ACG/ObjBossEnemy.h
@@ -16,11 +16,11 @@ public:
     void Action();	        //アクション
     void Draw();	        //ドロー
 private:
-    float m_boss_x;	 // ボスX座標
-    float m_boss_y;	 // ボスY座標
-    float m_boss_vx; // ボスX軸方向のベクトル
-    float m_boss_vy; // ボスY軸方向のベクトル
+    float m_x;	 // ボスX座標
+    float m_y;	 // ボスY座標
+    float m_vx; // ボスX軸方向のベクトル
+    float m_vy; // ボスY軸方向のベクトル
 
-    int m_boss_hp;   // ボスのＨＰ
+    int m_hp;   // ボスのＨＰ
 
 };
