b3ad38cfafc5712dbb7f3f2da70271b1138401c7
nakano <h1201615016@hiratagakuen@onmicrosoft.com>
2017-08-30 15:38:38 +0900

    bossを倒したときに開くドアのクラス追加

---
+++ ACG/ACG/ObjDoor.cpp
@@ -0,0 +1,31 @@
+#include "GameL\DrawTexture.h"
+#include "GameL\SceneManager.h"
+#include "GameL\HitBoxManager.h"
+
+#include "GameHead.h"
+#include "ObjDoor.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//コンストラクタ
+CObjDoor::CObjDoor(float x, float y)
+{
+	m_x = x;
+	m_y = y;
+}
+
+//イニシャライズ
+void CObjDoor::Init()
+{
+}
+
+//アクション
+void CObjDoor::Action()
+{
+}
+
+//ドロー
+void CObjDoor::Draw()
+{
+}
