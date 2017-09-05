19011e55e9621db6c63204b2d3b3719be9c369cb
中本和明 <kcomdev@gmail.com>
2017-09-05 10:46:16 +0900

    敵にブロックのあたり判定を追加。

--- ACG/ACG/ObjEnemy.cpp
+++ ACG/ACG/ObjEnemy.cpp
@@ -16,8 +16,8 @@ CObjEnemy::CObjEnemy(int x, int y)
 	//マップの要素数を補間
 	m_first_x = x;
 	m_first_y = y;
-	m_px = x * ENEMY_SIZE;
-	m_py = y * ENEMY_SIZE;
+	m_x = x * ENEMY_SIZE;
+	m_y = y * ENEMY_SIZE;
 }
 
 //イニシャライズ
@@ -32,13 +32,18 @@ void CObjEnemy::Init()
 	m_ani_frame = 1;  //静止フレームを初期にする
 	m_ani_max_time = 4; //アニメーション間隔幅
 
+	m_speed = 0.5f;	//初期スピード
+
 	//当たり判定用HitBoxを作成
-	Hits::SetHitBox(this, m_px, m_py, ENEMY_SIZE,ENEMY_SIZE, ELEMENT_ENEMY, OBJ_ENEMY, 1);
+	Hits::SetHitBox(this, m_x, m_y, ENEMY_SIZE,ENEMY_SIZE, ELEMENT_ENEMY, OBJ_ENEMY, 1);
 }
 
 //アクション
 void CObjEnemy::Action()
 {
+	//ブロック情報を持ってくる
+	CObjBlock* obj_b = (CObjBlock*)Objs::GetObj(OBJ_BLOCK);
+
 	//移動----------------------------------------------
 
 	//デバッグ用にボタン操作で動くようにしてます。
@@ -48,16 +53,16 @@ void CObjEnemy::Action()
 	if (Input::GetVKey('J') == true)
 	{
 		m_posture = 0.0f;
-		m_vx -= 5.0f;
-		m_ani_time += 1;
+		m_vx -= m_speed;
+		m_ani_time ++;
 	}
 
 	//Dキーがおされたとき：右移動
 	else if (Input::GetVKey('L') == true)
 	{
 		m_posture = 1.0f;
-		m_vx += 5.0f;
-		m_ani_time += 1;
+		m_vx += m_speed;
+		m_ani_time ++;
 	}
 	else
 	{
@@ -77,20 +82,17 @@ void CObjEnemy::Action()
 	{
 		m_ani_frame = 0;
 	}
-
+	
 	//摩擦
-	//m_vx += -(m_vx * 0.098);
+	m_vx += -(m_vx * 0.098);
 
 	//自由落下運動
-	//m_vy += 9.8 / (16.0f);
+	m_vy += 9.8 / (16.0f);
 
-	//移動ベクトルをポジションに加算
-	m_px += m_vx;
-	m_py += m_vy;
 
 	//移動ベクトルを初期化
-	m_vx = 0.0f;
-	m_vy = 0.0f;
+	/*m_vx = 0.0f;
+	m_vy = 0.0f;*/
 
 	//移動終わり-----------------------------------------
 
@@ -110,9 +112,20 @@ void CObjEnemy::Action()
 		
 		return;
 	}
+	
+	//ブロックとのあたり判定
+	obj_b->BlockHit(
+		&m_x, &m_y, ENEMY_SIZE, ENEMY_SIZE, false,
+		&m_hit_up, &m_hit_down, &m_hit_left, &m_hit_right, 
+		&m_vx, &m_vy
+	);
+
+	//移動ベクトルをポジションに加算
+	m_x += m_vx;
+	m_y += m_vy;
 
 	//HitBoxの位置を更新する
-	HitBoxUpData(Hits::GetHitBox(this), m_px, m_py);
+	HitBoxUpData(Hits::GetHitBox(this), m_x, m_y);
 
 	
 }
@@ -141,9 +154,9 @@ void CObjEnemy::Draw()
 	src.m_bottom = 64.0f;
 
 	//描画位置
-	dst.m_top = 0.0f + m_py - obj_m->GetScrollY();
-	dst.m_left = (ENEMY_SIZE * m_posture) + m_px - obj_m->GetScrollX();
-	dst.m_right = (ENEMY_SIZE - ENEMY_SIZE * m_posture) + m_px - obj_m->GetScrollX();
+	dst.m_top = 0.0f + m_y - obj_m->GetScrollY();
+	dst.m_left = (ENEMY_SIZE * m_posture) + m_x - obj_m->GetScrollX();
+	dst.m_right = (ENEMY_SIZE - ENEMY_SIZE * m_posture) + m_x - obj_m->GetScrollX();
 	dst.m_bottom = dst.m_top + ENEMY_SIZE;
 
 	//描画
