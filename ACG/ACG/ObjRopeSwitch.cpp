e6a0c066cc5416f2a5b29867ff0940d62638790a
nakano <h1201615016@hiratagakuen@onmicrosoft.com>
2017-09-07 14:34:10 +0900

    ロープスイッチの描画

--- ACG/ACG/ObjRopeSwitch.cpp
+++ ACG/ACG/ObjRopeSwitch.cpp
@@ -38,5 +38,26 @@ void CObjRopeSwitch::Action()
 //ドロー
 void CObjRopeSwitch::Draw()
 {
+	//描画カラー
+	float color[4] = { 1.0f,1.0f,1.0f, 1.0f };
 
+	RECT_F src, dst;
+
+	//マップオブジェクトを持ってくる
+	CObjMap* obj_m = (CObjMap*)Objs::GetObj(OBJ_MAP);
+
+	//切り取り位置
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 64.0f;
+	src.m_bottom = 64.0f;
+
+	//描画位置
+	dst.m_top = 0.0f + m_py - obj_m->GetScrollY();
+	dst.m_left = 0.0f + m_px - obj_m->GetScrollX();
+	dst.m_right = dst.m_left + ROPE_SWITCH_SIZE;
+	dst.m_bottom = dst.m_top + ROPE_SWITCH_SIZE;
+
+	//描画
+	Draw::Draw(13, &src, &dst, color, 0);
 }
