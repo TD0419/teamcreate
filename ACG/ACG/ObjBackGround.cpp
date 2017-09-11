b3adfe1ba47f5aedcc9381b5b0236ca2d7aad98d
中本和明 <kcomdev@gmail.com>
2017-09-11 15:02:00 +0900

    BackGroundを画像の大きさ通りに描画する(画面の左下に画像の左下を持ってくるように) をした

--- ACG/ACG/ObjBackGround.cpp
+++ ACG/ACG/ObjBackGround.cpp
@@ -31,6 +31,9 @@ void CObjBackGround::Action()
 //ドロー
 void CObjBackGround::Draw()
 {
+	//マップオブジェクトを持ってくる
+	CObjMap* obj_m = (CObjMap*)Objs::GetObj(OBJ_MAP);
+
 	//描画カラー
 	float color[4] = { 1.0f,1.0f,1.0f, 1.0f };
 
@@ -39,14 +42,14 @@ void CObjBackGround::Draw()
 	//切り取り位置
 	src.m_top = 0.0f;
 	src.m_left =0.0f;
-	src.m_right =src.m_left + 1024;
-	src.m_bottom = src.m_top + 1024;
+	src.m_right =src.m_left + 1000;
+	src.m_bottom = src.m_top + 100;
 
 	//描画位置
-	dst.m_top = 0.0f;
-	dst.m_left = 0.0f;
-	dst.m_right = dst.m_left + WINDOW_SIZE_W;
-	dst.m_bottom = dst.m_top + WINDOW_SIZE_H;
+	dst.m_top =  WINDOW_SIZE_H - src.m_bottom - src.m_top;
+	dst.m_left = WINDOW_SIZE_W - src.m_right - src.m_left;
+	dst.m_right = WINDOW_SIZE_W;
+	dst.m_bottom =  WINDOW_SIZE_H;
 
 	//描画
 	Draw::Draw(15, &src, &dst, color, 0.0f);
