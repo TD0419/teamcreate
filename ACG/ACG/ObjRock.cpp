1cb19dc0e508a018f84368e96e01f7f964a616ef
yagi <h1201615004@hiratagakuen.onmicrosoft.com>
2017-09-11 14:09:07 +0900

    とりま

--- ACG/ACG/ObjRock.cpp
+++ ACG/ACG/ObjRock.cpp
@@ -20,6 +20,10 @@ CObjRock::CObjRock(int x, int y)
 //イニシャライズ
 void CObjRock::Init()
 {
+	m_ani_time = 0;
+	m_ani_frame = 1;  //静止フレームを初期にする
+	m_ani_max_time = 17; //アニメーション間隔幅
+
 	//当たり判定用HitBoxを作成                          
 	Hits::SetHitBox(this, m_px, m_py, ROCK_SIZE_WIDTH, ROCK_SIZE_HEIGHT, ELEMENT_GIMMICK, OBJ_ROCK, 1);
 }
@@ -36,14 +40,29 @@ void CObjRock::Action()
 	//弾と接触しているかどうかを調べる　//ObjEnemyと同じようにしたほうがいいかも知れない。
 	if (hit->CheckObjNameHit(OBJ_BULLET) != nullptr)
 	{
-		this->SetStatus(false);		//自身に削除命令を出す
-		Hits::DeleteHitBox(this);	//岩が所有するHitBoxに削除する
+		
+		m_ani_time += 1;
 		return;
 	}
+
+	//アニメーションの感覚管理
+	if (m_ani_time > m_ani_max_time)
+	{
+		m_ani_frame += 1;
+		m_ani_time = 0;
+	}
+
+	//最後までアニメーションが進むと最初に戻る
+	if (m_ani_frame == 2)
+	{
+		this->SetStatus(false);		//自身に削除命令を出す
+		Hits::DeleteHitBox(this);	//岩が所有するHitBoxに削除する
+	}
+
 	HIT_DATA** hit_data;	//衝突の情報を入れる構造体
 	hit_data = hit->SearchObjNameHit(OBJ_HERO);//衝突の情報をhit_dataに入れる
 
-											   //主人公オブジェクトを持ってくる
+	//主人公オブジェクトを持ってくる
 	CObjHero* obj_hero = (CObjHero*)Objs::GetObj(OBJ_HERO);
 
 	for (int i = 0; i < hit->GetCount(); i++)
@@ -88,6 +107,12 @@ void CObjRock::Action()
 //ドロー
 void CObjRock::Draw()
 {
+	//画像の切り取り配列
+	int AniData[2] =
+	{
+		0  , 1 ,
+	};
+
 	//描画カラー
 	float color[4] = { 1.0f,1.0f,1.0f, 1.0f };
 
@@ -97,10 +122,10 @@ void CObjRock::Draw()
 	CObjMap* obj_m = (CObjMap*)Objs::GetObj(OBJ_MAP);
 
 	//切り取り位置
-	src.m_top = 1.0f;
-	src.m_left = 0.0f;
-	src.m_right = 64.0f;
-	src.m_bottom = 64.0f;
+	src.m_top = 0.0f;
+	src.m_left = AniData[m_ani_frame] * ROCK_SIZE_WIDTH;
+	src.m_right = src.m_left + ROCK_SIZE_WIDTH;
+	src.m_bottom = src.m_top + ROCK_SIZE_HEIGHT;
 	
 	//描画位置
 	dst.m_top = m_py - obj_m->GetScrollY()-128;
@@ -108,6 +133,7 @@ void CObjRock::Draw()
 	dst.m_right = dst.m_left + ROCK_SIZE_WIDTH;
 	dst.m_bottom = dst.m_top + ROCK_SIZE_HEIGHT;
 
+	
 	//描画
 	Draw::Draw(10, &src, &dst, color, 0.0f);
 }
