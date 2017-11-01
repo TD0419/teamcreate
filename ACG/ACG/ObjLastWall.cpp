75603a8392c3943d033dfb23763bef1ec9890e98
yagi <h1201615004@hiratagakuen.onmicrosoft.com>
2017-11-01 12:09:29 +0900

    不毛な壁修正

--- ACG/ACG/ObjLastWall.cpp
+++ ACG/ACG/ObjLastWall.cpp
@@ -44,28 +44,24 @@ void CObjLastWall::Action()
 
 	//マップオブジェクトを持ってくる
 	CObjMap* objmap = (CObjMap*)Objs::GetObj(OBJ_MAP);
-	int map_num = objmap->GetMap(m_map_x, m_map_y - 1);
+	int map_num = objmap->GetMap(m_map_x, m_map_y);
 
 	// m_wall_gaugeが512を越えたら処理ストップ
 	if (m_wall_gauge >= 512)
 	{
-		Hits::DeleteHitBox(this);//hitbox削除
-		objmap->SetMap(m_map_x, m_map_y, MAP_SPACE);//マップの数値を空にする
-		this->SetStatus(false);//自身
+		
 		return;
 	}
 	else
 	{//レバースイッチが押されていたら
 		if (m_look_unlock_flag == true)
 		{
-			m_wall_gauge += 0.2f; // 1ずつ増やしていく
+			m_wall_gauge += 1; // 1ずつ増やしていく
 		}
 	}
 
 	// hitboxが小さくなる
-	hit->SetPos(m_px - objmap->GetScrollX(), m_py - objmap->GetScrollY() + m_wall_gauge, WATER_SIZE_HEIGHT - m_wall_gauge, WATER_SIZE_WIDTH);
-
-
+	hit->SetPos(m_px - objmap->GetScrollX(), m_py - objmap->GetScrollY() + m_wall_gauge, 512 - m_wall_gauge, 32);
 
 	for (int i = 0; i < hit->GetCount(); i++)
 	{
@@ -135,15 +131,16 @@ void CObjLastWall::Draw()
 	Draw::Draw(21, &src, &dst, color, 0.0f);
 
 	//-----------------------------------------------------
-
-	//切り取り位置
-	src.m_top = 0.0;
-	src.m_left = 0.0f;
-	src.m_right = 32.0f;
-	src.m_bottom = 512.0f;
-
+	if (m_wall_gauge != 512)
+	{
+		//切り取り位置
+		src.m_top = 0.0 + m_wall_gauge;
+		src.m_left = 0.0f;
+		src.m_right = 32.0f;
+		src.m_bottom = 512.0f;
+	}
 	//描画位置
-	dst.m_top = m_py + m_wall_gauge - objmap->GetScrollY() + 65;
+	dst.m_top = m_py  - objmap->GetScrollY() + 65;
 	dst.m_left = m_px - objmap->GetScrollX() + 30;
 	dst.m_right = dst.m_left + 32;
 	dst.m_bottom = dst.m_top + 512 - m_wall_gauge;
