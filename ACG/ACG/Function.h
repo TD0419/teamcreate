afdff00245e2bb506816ef6ceded29b255317448
中本和明 <kcomdev@gmail.com>
2017-11-06 15:56:59 +0900

    LastWallのHitBoxがおかしかったので修正したけどその前に修正されてた。 関数を作ったのでそれに変更しました。

--- ACG/ACG/Function.h
+++ ACG/ACG/Function.h
@@ -4,6 +4,6 @@ using namespace GameL;
 
 void HitBoxUpData(CHitBox* hit,float m_px,float m_py);//HitBoxの位置を更新する関数
 
-void HitBoxUpData(CHitBox* hit, float m_px, float m_py,float h,float w);//HitBoxの位置と大きさを更新する関数
+void HitBoxUpData(CHitBox* hit, float m_px, float m_py,float w,float h);//HitBoxの位置と大きさを更新する関数
 
 bool WindowCheck(float m_px, float m_py,float obj_size_x,float obj_size_y);//画面の外に出ているか判定する関数
