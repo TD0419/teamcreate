31fe3a7d9efc22e586cb06d6bb0a89ee26ae57ec
yagi <h1201615004@hiratagakuen.onmicrosoft.com>
2017-12-04 16:23:43 +0900

    クリアの音をなるようにした

--- ACG/ACG/GameHead.h
+++ ACG/ACG/GameHead.h
@@ -343,5 +343,5 @@ enum MUSIC
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneTitle
+#define SET_GAME_START CSceneGameClear
 //-----------------------------------------------
