6c7c04f1f40a8b6fe69d12620a8230cb8e071936
中本和明 <kcomdev@gmail.com>
2017-12-19 15:06:22 +0900

    ステージ５のボス消滅アニメーション後ドアを作成しました

--- ACG/ACG/ObjAfterBossDisappearance.cpp
+++ ACG/ACG/ObjAfterBossDisappearance.cpp
@@ -12,7 +12,15 @@ using namespace GameL;
 //イニシャライズ
 void CObjAfterBossDisappearance::Init()
 {
+	//マップ情報を取得
+	CObjMap* objmap = (CObjMap*)Objs::GetObj(OBJ_MAP);
 
+	//マップ情報にドアを追加
+	objmap->SetMap(108,18, MAP_GOAL_DOOR);
+
+	//ドアを作成
+	CObjDoor* objDoor = new CObjDoor(108, 18);
+	Objs::InsertObj(objDoor, OBJ_DOOR, 9);
 }
 
 //アクション
